ad30677a6b9b6e588e318ddcc6e0e0becd6a10bb
ddavidad <ddavidad@gmail.com>
2014-12-20 15:27:53 +0200

    adding support for all elements (not just links) and fixing position problems when using with any type of element

--- tooltips.js
+++ tooltips.js
@@ -5,64 +5,67 @@
   $.fn.tooltips = function(el) {
 
     var $tooltip,
-      $body = $('body'),
-      $el;
+        $body = $('body'),
+        $el;
 
     // Ensure chaining works
     return this.each(function(i, el) {
-    
+
       $el = $(el).attr("data-tooltip", i);
 
       // Make DIV and append to page 
       var $tooltip = $('<div class="tooltip" data-tooltip="' + i + '">' + $el.attr('title') + '<div class="arrow"></div></div>').appendTo("body");
 
       // Position right away, so first appearance is smooth
-      var linkPosition = $el.position();
+      var elementRect = el.getBoundingClientRect();
+
 
       $tooltip.css({
-        top: linkPosition.top - $tooltip.outerHeight() - 13,
-        left: linkPosition.left - ($tooltip.width()/2)
+        // modulo added to prevent scrolling overlay issues
+        top: (elementRect.top - $tooltip.outerHeight() - 13) % window.innerHeight,
+        left: (elementRect.left + ((elementRect.right - elementRect.left)/2) - ($tooltip.width()/2))
       });
 
       $el
-      // Get rid of yellow box popup
-      .removeAttr("title")
+        // Get rid of yellow box popup
+          .removeAttr("title")
 
-      // Mouseenter
-      .hover(function() {
+        // Mouseenter
+          .hover(function() {
 
-        $el = $(this);
+            $el = $(this);
 
-        $tooltip = $('div[data-tooltip=' + $el.data('tooltip') + ']');
+            $tooltip = $('div[data-tooltip=' + $el.data('tooltip') + ']');
 
-        // Reposition tooltip, in case of page movement e.g. screen resize                        
-        var linkPosition = $el.position();
+            // Reposition tooltip, in case of page movement e.g. screen resize
+            var elementRect = el.getBoundingClientRect();
 
-        $tooltip.css({
-          top: linkPosition.top - $tooltip.outerHeight() - 13,
-          left: linkPosition.left - ($tooltip.width()/2)
-        });
+            $tooltip.css({
+              // the element in page position - tooltip height - 15 + scroll Y parameter(with compatibility)
+              top: elementRect.top - $tooltip.outerHeight() -  15 + ((window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop),
+              left: elementRect.left + ((elementRect.right - elementRect.left)/2) - ($tooltip.width()/2) - 19
+            });
 
-        // Adding class handles animation through CSS
-        $tooltip.addClass("active");
+            // Adding class handles animation through CSS
+            $tooltip.addClass("active");
 
-        // Mouseleave
-      }, function() {
+            // Mouseleave
+          }, function() {
 
-        $el = $(this);
+            $el = $(this);
 
-        // Temporary class for same-direction fadeout
-        $tooltip = $('div[data-tooltip=' + $el.data('tooltip') + ']').addClass("out");
+            // Temporary class for same-direction fadeout
+            $tooltip = $('div[data-tooltip=' + $el.data('tooltip') + ']').addClass("out");
 
-        // Remove all classes
-        setTimeout(function() {
-          $tooltip.removeClass("active").removeClass("out");
-          }, 300);
+            // Remove all classes
+            setTimeout(function() {
+              $tooltip.removeClass("active").removeClass("out");
+            }, 300);
 
-        });
+          });
 
-      });
+    });
 
-    }
+  }
 
 })(jQuery);
